8d230535e501f331991249492b40eef5453f5dfb
Jhon Lopez <jhonlv10@gmail.com>
2025-05-29 23:09:55 -0500

    first commit of library app with java SPRING BOOT, MySQL, HTML

---
+++ src/main/java/com/egg/biblioteca/SeguridadWeb.java
@@ -0,0 +1,47 @@
+package com.egg.biblioteca;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SeguridadWeb {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/usuarios/**").authenticated()
+                .requestMatchers("/css/", "/js/", "/img/", "/**").permitAll()
+                .requestMatchers("/login","/registro","/registrar").permitAll()
+                .anyRequest().authenticated())
+            .formLogin((form) -> form
+                .loginPage("/login")
+                .loginProcessingUrl("/logincheck")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/inicio",true)
+                .permitAll())
+            .logout((logout) -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll())
+            .csrf(csrf -> csrf
+                .disable());
+
+        return httpSecurity.build();
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
